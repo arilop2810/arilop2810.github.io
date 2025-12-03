ae5808069b5657bb637c133057ae90d2f0dead64
arilop2810 <ariannalopez2810@gmail.com>
2025-12-03 02:06:17 -0600

    Final Project AME 220
    Final Project upload. Get to know Jay, an interactive slideshow gallery.

---
+++ Final/SlideTrial.js
@@ -0,0 +1,45 @@
+//keep track of active slides
+//show only one slide at a time
+//highlights thumnail
+//updates caption
+
+
+
+//set slideshow to show the first image once the page loads//
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("JaySlidesImages");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+
+  //logic
+  //if you go past last slide...go back to slide 1
+  //if you go before slide one..go to last slide
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  //hides all slides first down here
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  //highlights active thumnail image
+  dots[slideIndex-1].className += " active";
+  //sets the caption text
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
